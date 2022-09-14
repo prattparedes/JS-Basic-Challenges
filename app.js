5156a17282e282bd582efe7e8ff114d0a4b64f71
Pratt <pratt.paredes@gmail.com>
2022-09-13 23:53:51 -0500

    area, string, conversion

---
+++ app.js
@@ -0,0 +1,35 @@
+// Q1. Return the sum of two numbers
+// Create a function that takes two arguments as functions and return their sum
+/**@example
+ * addition(3, 2) -> 5
+ * addition(-3, -6) -> -9
+ * addition(7, 3) -> 10
+ */
+
+function addition(a, b) {
+    return a + b
+}
+
+console.log(addition(5, 5))
+
+// Convertir horas en  segundos
+function converthour(num1) {
+    return num1 * 3600
+}
+
+console.log(converthour(2))
+
+//calcualr area de triangulo
+
+function AreaTriangulo(base, altura) {
+    return (base * altura)/2
+}
+
+console.log(AreaTriangulo(15, 3))
+
+//Extender un string
+function extenderString(texto) {
+    let texto = ''
+}
+
+console.log('OWO')t
